46b2a3f93d41e8088c03f884b5346a1f21e49566
amiceli <antoine@miceli.click>
2024-03-09 10:50:43 +0100

    feat: allow relative file for loadFeatures

--- src/vitest/load-feature.ts
+++ src/vitest/load-feature.ts
@@ -3,21 +3,28 @@ import { FeatureFileReader } from "../parser/readfile"
 import callsites from 'callsites'
 import { dirname } from 'path'
 
+function getCallerPath () : string | null {
+    const { 2 : callerFilePath } = callsites()
+    const callerFileName = callerFilePath?.getFileName() || ``
+    const callerFileDir = dirname(callerFileName)
+
+    return callerFileDir
+}
 /**
  * @deprecated You can use it but multiple features in Gherkin file is not a good practice.
  */
 export async function loadFeatures (featureFilePath: string): Promise<Readonly<Feature[]>> {
+    const callerFileDir = getCallerPath()
+
     const features = await FeatureFileReader
-        .fromPath(featureFilePath)
+        .fromPath(featureFilePath, callerFileDir)
         .parseFile()
 
     return features
 }
 
 export async function loadFeature (featureFilePath: string): Promise<Readonly<Feature>> {
-    const [, callerFilePath] = callsites()
-    const callerFileName = callerFilePath?.getFileName() || ``
-    const callerFileDir = dirname(callerFileName)
+    const callerFileDir = getCallerPath()
 
     const [feature] = await FeatureFileReader
         .fromPath(featureFilePath, callerFileDir)
