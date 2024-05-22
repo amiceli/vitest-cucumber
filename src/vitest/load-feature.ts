83e3490dcb6aa0d3e1bf64ebe9894d4870ab8d84
amiceli <antoine@miceli.click>
2024-05-22 22:30:22 +0200

    fix: detect feature file path checking stack path
    - update regex to detect relative path
    - check all callsites path to check feature file path

--- src/vitest/load-feature.ts
+++ src/vitest/load-feature.ts
@@ -2,19 +2,26 @@ import { Feature } from "../parser/feature"
 import { FeatureFileReader } from "../parser/readfile"
 import callsites from 'callsites'
 import { dirname } from 'path'
+import fs from 'fs'
 
-function getCallerPath () : string | null {
-    const { 2 : callerFilePath } = callsites()
-    const callerFileName = callerFilePath?.getFileName() || ``
-    const callerFileDir = dirname(callerFileName)
+function getCallerPath (filePath : string): string | null {
+    const list = callsites()
 
-    return callerFileDir
+    return list.map((s) => {
+        const callerFileName = s?.getFileName() || ``
+        const callerFileDir = dirname(callerFileName)
+
+        return callerFileDir
+    }).find((s) => {
+        return fs.existsSync(`${s}/${filePath}`)
+    }) || null
 }
+
 /**
  * @deprecated You can use it but multiple features in Gherkin file is not a good practice.
  */
 export async function loadFeatures (featureFilePath: string): Promise<Readonly<Feature[]>> {
-    const callerFileDir = getCallerPath()
+    const callerFileDir = getCallerPath(featureFilePath)
 
     const features = await FeatureFileReader
         .fromPath(featureFilePath, callerFileDir)
@@ -24,7 +31,7 @@ export async function loadFeatures (featureFilePath: string): Promise<Readonly<F
 }
 
 export async function loadFeature (featureFilePath: string): Promise<Readonly<Feature>> {
-    const callerFileDir = getCallerPath()
+    const callerFileDir = getCallerPath(featureFilePath)
 
     const [feature] = await FeatureFileReader
         .fromPath(featureFilePath, callerFileDir)
