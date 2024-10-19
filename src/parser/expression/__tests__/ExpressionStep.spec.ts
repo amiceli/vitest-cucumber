1d2faf23e8021a6e7ddda950d8dd7e46deb0326a
Odonno <bottiau.david@laposte.net>
2024-10-19 13:19:50 +0200

    test: rework existing tests for ExpressionStep

--- src/parser/expression/__tests__/ExpressionStep.spec.ts
+++ src/parser/expression/__tests__/ExpressionStep.spec.ts
@@ -4,43 +4,86 @@ import { Step, StepTypes } from '../../models/step'
 import { ExpressionStep } from '../ExpressionStep'
 
 describe(`ExpressionStep`, () => {
-    it(`should match {string}`, () => {
-        const step = new Step(StepTypes.GIVEN, `I love 'Vue'`)
-        const params = ExpressionStep.matchStep(step, `I love {string}`)
-        expect(params).toEqual([`Vue`])
+    describe('{string}', () => {
+        it(`should match {string}`, () => {
+            const step = new Step(StepTypes.GIVEN, `I love 'Vue'`)
+            const params = ExpressionStep.matchStep(step, `I love {string}`)
+            expect(params).toEqual([`Vue`])
+        })
+
+        it(`should match multiple {string}`, () => {
+            const step = new Step(StepTypes.GIVEN, `I love 'Vue' for "web"`)
+            const params = ExpressionStep.matchStep(
+                step,
+                `I love {string} for {string}`,
+            )
+            expect(params).toEqual([`Vue`, `web`])
+        })
     })
 
-    it(`should match multiple {string}`, () => {
-        const step = new Step(StepTypes.GIVEN, `I love 'Vue' for "web"`)
-        const params = ExpressionStep.matchStep(
-            step,
-            `I love {string} for {string}`,
-        )
-        expect(params).toEqual([`Vue`, `web`])
+    describe('{number}', () => {
+        it(`should match {number}`, () => {
+            const step = new Step(StepTypes.GIVEN, `I love Vue 3`)
+            const params = ExpressionStep.matchStep(step, `I love Vue {number}`)
+            expect(params).toEqual([3])
+        })
+
+        it.skip(`should match multiple {number}`, () => {
+            const step = new Step(StepTypes.GIVEN, `I love Vue 3.1 or 3.2`)
+            const params = ExpressionStep.matchStep(
+                step,
+                `I love Vue {number} or {number}`,
+            )
+            expect(params).toEqual([3.1, 3.2])
+        })
     })
 
-    it(`should match {number}`, () => {
-        const step = new Step(StepTypes.GIVEN, `I love Vue 3`)
-        const params = ExpressionStep.matchStep(step, `I love Vue {number}`)
-        expect(params).toEqual([3])
+    describe('{float}', () => {
+        it(`should match {float}`, () => {
+            const step = new Step(StepTypes.GIVEN, `I love Vue 3.1`)
+            const params = ExpressionStep.matchStep(step, `I love Vue {float}`)
+            expect(params).toEqual([3.1])
+        })
+
+        it(`should match multiple {float}`, () => {
+            const step = new Step(StepTypes.GIVEN, `I love Vue 3.1 or 3.2`)
+            const params = ExpressionStep.matchStep(
+                step,
+                `I love Vue {float} or {float}`,
+            )
+            expect(params).toEqual([3.1, 3.2])
+        })
     })
 
-    it(`should match {float}`, () => {
-        const step = new Step(StepTypes.GIVEN, `I love Vue 3.1`)
-        const params = ExpressionStep.matchStep(step, `I love Vue {float}`)
-        expect(params).toEqual([3.1])
+    describe('{list}', () => {
+        it(`should match {list}`, () => {
+            const step = new Step(
+                StepTypes.GIVEN,
+                `I use React, Astro, TypeScript`,
+            )
+            const params = ExpressionStep.matchStep(step, `I use {list}`)
+            const expectedList = ['React', 'Astro', 'TypeScript']
+
+            expect(params).toEqual([expectedList])
+        })
+
+        it(`should match multiple {list}`, () => {
+            const step = new Step(
+                StepTypes.GIVEN,
+                `I use React, Astro, TypeScript and I also use Vue, Svelte, Angular`,
+            )
+            const params = ExpressionStep.matchStep(
+                step,
+                `I use {list} and I also use {list}`,
+            )
+            const expectedList1 = ['React', 'Astro', 'TypeScript']
+            const expectedList2 = ['Vue', 'Svelte', 'Angular']
+
+            expect(params).toEqual([expectedList1, expectedList2])
+        })
     })
 
-    it(`should match many {float}`, () => {
-        const step = new Step(StepTypes.GIVEN, `I love Vue 3.1 or 3.2`)
-        const params = ExpressionStep.matchStep(
-            step,
-            `I love Vue {float} or {float}`,
-        )
-        expect(params).toEqual([3.1, 3.2])
-    })
-
-    it(`should match {string} and {number}`, () => {
+    it(`should match both {string} and {number}`, () => {
         const step = new Step(StepTypes.GIVEN, `I love "Vue" 3 and 12`)
         const params = ExpressionStep.matchStep(
             step,
