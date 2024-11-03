8dfab9d0b6b6de24b1dab50e6d80bc425ab794ef
amiceli <antoine@miceli.click>
2024-11-03 18:40:06 +0100

    test: add unit for vitest plugin for removed scenario

--- src/plugin/__tests__/vitest-plugin.feature
+++ src/plugin/__tests__/vitest-plugin.feature
@@ -4,23 +4,24 @@ Feature: vitest-cucumber plugin
         Given My feature files are in "src/__tests__/"
         And   My spec files are in "src/__tests__/"
 
-    Scenario: Create spec file for new feature file
-        Given "src/__tests__/awesome.spec.ts" doesn't exists
-        When  I write "src/__tests__/awesome.feature"
-        Then  vitest-cucumber create "src/__tests__/awesome.spec.ts"
+    # Scenario: Create spec file for new feature file
+    #     Given "src/__tests__/awesome.spec.ts" doesn't exists
+    #     When  I write "src/__tests__/awesome.feature"
+    #     Then  vitest-cucumber create "src/__tests__/awesome.spec.ts"
 
     Scenario: Add scenario to spec file
         Given "src/__tests__/awesome.spec.ts" hasn't scenario
         When  I add a scenario into "src/__tests__/awesome.feature"
         """
-        Scenario: example
-            Given I'm new scenario
-            Then  I'm added into spec file
+        Feature: example
+            Scenario: new scenario
+                Given I'm new scenario
+                Then  I'm added into spec file
         """
         Then  vitest-cucumber add new scenario in "src/__tests__/awesome.spec.ts"
 
     Scenario: Remove scenario in feature file
-        Given "src/__tests__/awesome.feature" as "example" scenario
-        When  I remove "example" scenario in feature file
+        Given 'src/__tests__/awesome.feature' has "example" scenario
+        When  I remove "example" scenario in "src/__tests__/awesome.feature"
         Then  vitest-cucumber remove "example" scenario in "src/__tests__/awesome.spec.ts"
 
--- src/plugin/__tests__/vitest-plugin.spec.ts
+++ src/plugin/__tests__/vitest-plugin.spec.ts
@@ -9,7 +9,7 @@ describeFeature(feature, ({ BeforeAllScenarios, Background, Scenario }) => {
     BeforeAllScenarios(() => {
         fs.rmSync('src/__tests__/awesome.spec.ts', { force: true })
         fs.rmSync('src/__tests__/awesome.feature', { force: true })
-        fs.rmdirSync('src/__tests__/', { recursive: true })
+        fs.rmSync('src/__tests__/', { recursive: true })
 
         fs.mkdirSync('src/__tests__/')
         fs.writeFileSync('src/__tests__/awesome.spec.ts', '')
@@ -29,10 +29,6 @@ describeFeature(feature, ({ BeforeAllScenarios, Background, Scenario }) => {
         )
         And(`My spec files are in {string}`, (ctx, specPath: string) => {
             specFilePath = specPath
-            // expect(fs.readdirSync('src/__tests__/')).toEqual([
-            //     'awesome.feature',
-            //     'awesome.spec.ts',
-            // ])
             featureAst = FeatureAst.fromOptions({
                 specFilePath: 'src/__tests__/awesome.spec.ts',
                 featureFilePath: 'src/__tests__/awesome.feature',
@@ -40,46 +36,69 @@ describeFeature(feature, ({ BeforeAllScenarios, Background, Scenario }) => {
         })
     })
 
-    Scenario(
-        `Create spec file for new feature file`,
-        ({ Given, When, Then }) => {
-            Given(`{string} doesn't exists`, (ctx, specPath: string) => {
-                expect(fs.readFileSync(specPath).toString()).toEqual('')
-            })
-            When(`I write {string}`, (ctx, faturePath: string) => {
-                const content = [
-                    'Feature: test',
-                    '   Scenario: example',
-                    '       Given I am a step',
-                ].join('\n')
-                fs.writeFileSync(faturePath, content)
-            })
-            Then(
-                `vitest-cucumber create {string}`,
-                async (ctx, specPath: string) => {
-                    await featureAst.updateSpecFile()
-                    const content = fs.readFileSync(specPath).toString()
-
-                    expect(content.includes('Scenario(`example`')).toBe(true)
-                    expect(content.includes('Given(`I am a step`')).toBe(true)
-                },
-            )
-        },
-    )
     Scenario(`Add scenario to spec file`, ({ Given, When, Then }) => {
-        Given(`"src/__tests__/awesome.spec.ts" hasn't scenario`, () => {})
-        When(`I add a scenario into "src/__tests__/awesome.feature"`, () => {})
+        Given(`{string} hasn't scenario`, (ctx, specPath: string) => {
+            fs.writeFileSync(specPath, '')
+        })
+        When(
+            `I add a scenario into {string}`,
+            (ctx, featurePath: string, docsString: string) => {
+                fs.writeFileSync(featurePath, docsString)
+            },
+        )
         Then(
-            `vitest-cucumber add new scenario in "src/__tests__/awesome.spec.ts"`,
-            () => {},
+            `vitest-cucumber add new scenario in {string}`,
+            async (ctx, specPath: string) => {
+                await featureAst.updateSpecFile()
+                const content = fs.readFileSync(specPath).toString()
+
+                expect(content.includes('Scenario(`new scenario`')).toBe(true)
+            },
         )
     })
     Scenario(`Remove scenario in feature file`, ({ Given, When, Then }) => {
-        Given(`"src/__tests__/awesome.feature" as "example" scenario`, () => {})
-        When(`I remove "example" scenario in feature file`, () => {})
+        Given(
+            '{string} has {string} scenario',
+            async (ctx, featurePath: string, name: string) => {
+                fs.writeFileSync('src/__tests__/awesome.spec.ts', '')
+                fs.writeFileSync(
+                    featurePath,
+                    [
+                        'Feature: test',
+                        `   Scenario: ${name}`,
+                        '       Given I am a step',
+                        `   Scenario: another`,
+                        '       Given I am a step again',
+                    ].join('\n'),
+                )
+                await featureAst.updateSpecFile()
+            },
+        )
+        When(
+            `I remove {string} scenario in {string}`,
+            async (ctx, scenario: string, featurePath: string) => {
+                fs.writeFileSync(
+                    featurePath,
+                    [
+                        'Feature: test',
+                        `   Scenario: another`,
+                        '       Given I am a stepagain',
+                    ].join('\n'),
+                )
+                await featureAst.updateSpecFile()
+            },
+        )
         Then(
-            `vitest-cucumber remove "example" scenario in "src/__tests__/awesome.spec.ts"`,
-            () => {},
+            `vitest-cucumber remove {string} scenario in {string}`,
+            (ctx, scenario: string, spcedFile: string) => {
+                const content = fs.readFileSync(spcedFile).toString()
+                console.debug({
+                    content,
+                })
+
+                expect(content.includes(`Scenario(\`${scenario}\``)).toBe(false)
+                expect(content.includes(`another`)).toBe(true)
+            },
         )
     })
 })
