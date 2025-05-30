f7c685e03e0ad54fac4ad15b206a0d8f02c13610
amiceli <antoine@miceli.click>
2025-05-30 22:14:47 +0200

    fix: exit when missing args

--- scripts/cli.ts
+++ scripts/cli.ts
@@ -8,10 +8,12 @@ const { feature, spec, lang } = minimist(process.argv.slice(2))
 
 if (!feature) {
     console.error('--feature option is required')
+    process.exit(-1)
 }
 
 if (!spec) {
     console.error('--spec option is required')
+    process.exit(-1)
 }
 
 loadFeature(feature, { language: lang || 'en' }).then((f) => {
