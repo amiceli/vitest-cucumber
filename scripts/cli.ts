ebe471f355b95cc3bd8f12b96a685da8c7ac6ed0
amiceli <antoine@miceli.click>
2025-05-30 12:23:28 +0200

    feat: use args for cli script

--- scripts/cli.ts
+++ scripts/cli.ts
@@ -1,14 +1,23 @@
 #!/usr/bin/env node
 
+import minimist from 'minimist'
 import { loadFeature } from '../src/vitest/load-feature'
 import { writeSpecFile } from './generateFile'
 
-const [featureFilePath, specFilePath] = process.argv.slice(2)
+const { feature, spec, lang } = minimist(process.argv.slice(2))
 
-loadFeature(featureFilePath).then((feature) => {
+if (!feature) {
+    console.error('--feature option is required')
+}
+
+if (!spec) {
+    console.error('--spec option is required')
+}
+
+loadFeature(feature, { language: lang || 'en' }).then((f) => {
     writeSpecFile({
-        feature,
-        featureFilePath,
-        specFilePath,
+        feature: f,
+        featureFilePath: feature,
+        specFilePath: spec,
     })
 })
