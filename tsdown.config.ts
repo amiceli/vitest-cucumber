672d5630991d205343def1acbe32abf4c38011c9
amiceli <antoine@miceli.click>
2026-06-22 21:36:07 +0200

    feat: add new "load-feature" export

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,17 +1,36 @@
 import { defineConfig } from 'tsdown'
 
-export default defineConfig({
-    entry: {
-        module: 'src/module.ts',
-        browser: 'src/browser/index.ts',
-        plugin: 'src/plugin/index.ts',
+export default defineConfig([
+    {
+        name: 'lib',
+        entry: {
+            module: 'src/module.ts',
+            browser: 'src/browser/index.ts',
+            plugin: 'src/plugin/index.ts',
+            'load-feature': 'src/load-feature.ts',
+        },
+        format: 'esm',
+        dts: true,
+        clean: true,
+        deps: {
+            neverBundle: [
+                'vite',
+            ],
+        },
     },
-    format: 'esm',
-    dts: true,
-    clean: true,
-    deps: {
-        neverBundle: [
-            'vite',
-        ],
+    {
+        name: 'cli-generate',
+        entry: {
+            'cli-generate': 'scripts/cli.ts',
+        },
+        format: 'cjs',
+        dts: false,
+        clean: false,
+        outExtensions: () => ({
+            js: '.js',
+        }),
+        define: {
+            'import.meta': '({})',
+        },
     },
-})
+])
